386ff43d6028757654d6c8729c9946abe10eae2c
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-06-24 18:43:15 -0400

    Added migrations

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require('express');
 const helmet = require('helmet');
 const server = express();
 
+const knex = require('knex');
+const knexConfig = require('./knexfile')
+const db = knex(knexConfig.development);
+
 server.use(helmet());
 server.use(express.json());
+
 
 module.exports = server;
