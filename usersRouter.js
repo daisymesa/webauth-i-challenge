06d4f970676ea23f6957aca86219fca367eeeeae
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-06-25 18:49:18 -0400

    GET endpoint for registering users

--- usersRouter.js
+++ usersRouter.js
@@ -30,7 +30,25 @@ router.get('/', (req, res) => {
 //Creates a user using the information sent inside the body of the request. 
 //Hash the password before saving the user to the database.
 router.post('/register', (req, res) => {
+    let newUser = req.body;
+    const hash = bcrypt.hashSync(user.password, 8);
+    user.password = hash;
 
+    // const newUser = { 
+    //     username: req.body.name,
+    //     password: req.body.password
+    // }
+
+    db('users')
+        .insert(newUser)
+        .then(addedUser => {
+            res.status(201).json(addedUser);
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: 'The new user information could not be added.'
+            })
+        })
 })
 
 
@@ -39,7 +57,7 @@ router.post('/register', (req, res) => {
 //On successful login, create a new session for the user and send back a 'Logged in' message and a cookie that contains the user id. 
 //If login fails, respond with the correct status code and the message: 'You shall not pass!'
 router.post('/login', (req, res) => {
-    
+
 })
 
 
@@ -47,7 +65,7 @@ router.post('/login', (req, res) => {
 //If the user is logged in, respond with an array of all the users contained in the database. 
 //If the user is not logged in respond with the correct status code and the message: 'You shall not pass!'.
 router.get('/users', (req, res) => {
-    
+
 })
 
 
