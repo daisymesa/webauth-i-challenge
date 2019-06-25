dca638691f6f5996a9d431d587864cfbae68f324
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-06-25 17:05:14 -0400

    Adding endpoints initial setup

--- usersRouter.js
+++ usersRouter.js
@@ -5,14 +5,13 @@ const knex = require('knex');
 const knexConfig = require('./knexfile.js');
 const db = knex(knexConfig.development);
 
-
+/*
 router.get('/', (req, res) => {
     res.send(`
     <h2>Router is working!!</h2>
     `)
 })
-
-/*
+*/
 
 router.get('/', (req, res) => {
     db('users')
@@ -24,6 +23,30 @@ router.get('/', (req, res) => {
         })
 })
 
-*/
+
+// POST:
+//Creates a user using the information sent inside the body of the request. 
+//Hash the password before saving the user to the database.
+router.post('/register', (req, res) => {
+
+})
+
+
+// POST:
+//Use the credentials sent inside the body to authenticate the user. 
+//On successful login, create a new session for the user and send back a 'Logged in' message and a cookie that contains the user id. 
+//If login fails, respond with the correct status code and the message: 'You shall not pass!'
+router.post('/register', (req, res) => {
+    
+})
+
+
+// GET:
+//If the user is logged in, respond with an array of all the users contained in the database. 
+//If the user is not logged in respond with the correct status code and the message: 'You shall not pass!'.
+router.post('/register', (req, res) => {
+    
+})
+
 
 module.exports = router;
